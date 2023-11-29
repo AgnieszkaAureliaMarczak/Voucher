0392a6debec216da64bc25555e4e1866dd04f46c
AgnieszkaMarczak <agnieszka.aurelia.marczak@gmail.com>
2023-11-29 17:57:08 +0100

    Wprowadzanie danych

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj identyfikator oferty");
+        String id = scanner.nextLine();
+        System.out.println("Podaj ilosc nocy");
+        int numberOfNights = scanner.nextInt();
+
     }
 }
