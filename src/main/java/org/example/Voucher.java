219884c117e73656e6330968648144e2fdd33005
AgnieszkaMarczak <agnieszka.aurelia.marczak@gmail.com>
2023-12-05 13:47:40 +0100

    Voucher fields created

--- src/main/java/org/example/Voucher.java
+++ src/main/java/org/example/Voucher.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Voucher {
+    private String id;
+    private int numberOfNights;
+    private int numberOfPeople;
+    private String validity;
+    private String addOns;
+    private String roomType;
 }
