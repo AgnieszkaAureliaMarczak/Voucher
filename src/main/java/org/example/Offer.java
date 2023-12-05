25b4178f079ec6f5d12832332fb981c6db73b345
AgnieszkaMarczak <agnieszka.aurelia.marczak@gmail.com>
2023-12-05 15:17:41 +0100

    Offer fields created

--- src/main/java/org/example/Offer.java
+++ src/main/java/org/example/Offer.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Offer {
+    private String id;
+    private String name;
+    private List<Voucher> vouchers = new ArrayList<>();
+
 }
